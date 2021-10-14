801de6b5c48159660e8d6addf4bc141a9632e394
teddywlq <teddy.wlq@gmail.com>
2021-10-14 22:19:30 +0800

    Bump to v1.3.14

--- smi_drv.h
+++ smi_drv.h
@@ -42,11 +42,11 @@
 
 #define DRIVER_NAME		"smifb"
 #define DRIVER_DESC		"SiliconMotion GPU DRM Driver"
-#define DRIVER_DATE		"20210730"
+#define DRIVER_DATE		"20211001"
 
 #define DRIVER_MAJOR		1
 #define DRIVER_MINOR		3
-#define DRIVER_PATCHLEVEL	13
+#define DRIVER_PATCHLEVEL	14
 
 #define SMIFB_CONN_LIMIT 3
 
@@ -56,7 +56,7 @@
 #define SUPPORT_CHIP " \t\tSM750, SM768\n"
 
 
-#define _version_	"1.3.13.0"
+#define _version_	"1.3.14.0"
 
 #undef  NO_WC
 

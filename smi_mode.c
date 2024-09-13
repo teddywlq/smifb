614ea68f7e62216ce47a792d1684e816685f6ae3
Teddy Wang <59642778+teddywlq@users.noreply.github.com>
2024-09-13 23:27:36 +0800

    Add to filter the clock above 150MHz on DVI

--- smi_mode.c
+++ smi_mode.c
@@ -1109,10 +1109,9 @@ static enum drm_mode_status smi_connector_mode_valid(struct drm_connector *conne
 	}
 	
 	if(connector->connector_type == DRM_MODE_CONNECTOR_DVII){
-		if((mode->hdisplay > 1920) || (mode->vdisplay > 1080))
+		if((mode->hdisplay > 1920) || (mode->vdisplay > 1080) || (mode->clock > 150000))
 			return MODE_NOMODE;
-		else
-			return MODE_OK;
+
 	}
 
 

9bcabbd947c19e36ab89ad053eae25f9e4e0fbea
Teddy Wang <59642778+teddywlq@users.noreply.github.com>
2025-06-04 11:54:40 +0800

    Bump to v1.5.2

--- smi_drv.h
+++ smi_drv.h
@@ -38,11 +38,11 @@
 
 #define DRIVER_NAME		"smifb"
 #define DRIVER_DESC		"SiliconMotion GPU DRM Driver"
-#define DRIVER_DATE		"20250515"
+#define DRIVER_DATE		"20250604"
 
 #define DRIVER_MAJOR		1
 #define DRIVER_MINOR		5
-#define DRIVER_PATCHLEVEL	1
+#define DRIVER_PATCHLEVEL	2
 
 #define SMIFB_CONN_LIMIT 3
 
@@ -52,7 +52,7 @@
 #define SUPPORT_CHIP " SM750, SM768, SM770"
 
 
-#define _version_	"1.5.1.0"
+#define _version_	"1.5.2.0"
 
 #undef  NO_WC
 
